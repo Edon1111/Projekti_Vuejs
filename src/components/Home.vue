cc68ba314c6d22adabc2a1df41d1df73a3dcab4f
anilaavdylii <aa48857@ubt-uni.net>
2022-03-02 22:43:56 +0100

    updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,25 @@
 <template>
- <v-layout>
- <div v-text="message"></div>
+ <v-layout row wrap>
+ <v-flex xs12>
+ <div>{{message}}</div>
+ </v-flex>
+ <v-flex xs12>
+
+<v-btn color="primary" v-on:click="reply">Reply</v-btn>
+ </v-flex>
  </v-layout>
 </template>
 <script type="text/javascript">
-export default {
- data() {
+ export default {
+ data () {
  return {
- message: 'Hello there!!!!',
- };
+ message: 'Hello there, how are you?',
+ }
  },
-};
+ methods: {
+ reply () {
+ this.message = "I'm doing great. Thank You!"
+ }
+ }
+ }
 </script>
