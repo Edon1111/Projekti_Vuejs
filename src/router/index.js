b8b0832dd611dccbb1ecc87ca3cf9461440f46d8
Edon1111 <63924185+Edon1111@users.noreply.github.com>
2022-03-03 17:24:13 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/components/Home'
-import Contact from '@/components/Contact';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import Contact from "@/components/Contact";
+import AddMovie from "@/components/AddMovie";
+import Movie from "@/components/Movie";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
+
   routes: [
-  {
-  path: '/',
-  name: 'Home',
-  component: Home,
-  },
-  {
-  path: '/contact',
-  name: 'Contact',
-  component: Contact,
-  },
-  ],
- });
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: Contact
+    },
+    {
+      path: "/movies/add",
+      name: "AddMovie",
+      component: AddMovie
+    },
+    {
+      path: "/movies/:id",
+      name: "Movie",
+      component: Movie
+    }
+  ]
+});
