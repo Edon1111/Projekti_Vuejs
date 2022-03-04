7f1799dead9c5404daafc66d67d809d12f665356
Edon1111 <63924185+Edon1111@users.noreply.github.com>
2022-03-04 17:34:54 +0100

    Passport.js per Authentication

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,9 @@ import Router from "vue-router";
 import Home from "@/components/Home";
 import Contact from "@/components/Contact";
 import AddMovie from "@/components/AddMovie";
-import Movie from '@/components/Movie';
-import Register from '@/components/Register';
+import Movie from "@/components/Movie";
+import Register from "@/components/Register";
+import Login from "@/components/Login";
 Vue.use(Router);
 
 export default new Router({
@@ -27,15 +28,19 @@ export default new Router({
       component: AddMovie
     },
     {
-      path: '/movies/:id',
-      name: 'Movie',
-      component: Movie,
-      },
-      {
-        path: '/users/register',
-        name: 'Register',
-        component: Register,
-        },
-       
-  ],
+      path: "/movies/:id",
+      name: "Movie",
+      component: Movie
+    },
+    {
+      path: "/users/register",
+      name: "Register",
+      component: Register
+    },
+    {
+      path: "/users/login",
+      name: "Login",
+      component: Login
+    }
+  ]
 });
