9012006c03a13a8093ef379146b6cf81ac7b8a17
AltinaSalihu <75257807+AltinaSalihu@users.noreply.github.com>
2022-03-04 20:41:46 +0100

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  DB: "mongodb://localhost/movie_rating_app",
-  SECRET: "movieratingappsecretkey"
-};
+  DB: 'mongodb://localhost/movie_rating_app',
+  SECRET: 'movieratingappsecretkey',
+  FACEBOOK_APP_ID: '486183993083088',
+  FACEBOOK_APP_SECRET: '●●●●●●●●'
+ }
