68854f939ab0f9d48c61b30d71a8c79a5be3cf47
AltinaSalihu <75257807+AltinaSalihu@users.noreply.github.com>
2022-03-09 20:34:22 +0100

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,12 @@ import axios from "axios";
 Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
-    movies: []
+    movies: [],
+    contact: []
   },
   getters: {
-    fetchMovies: state => state.movies
+    fetchMovies: state => state.movies,
+    fetchContact: state => state.contact
   },
 
   mutations: {
@@ -16,8 +18,14 @@ export const store = new Vuex.Store({
     },
 
     MOVIES: (state, payload) => {
-      state.movies = payload;
-    }
+      state.movies = payload; 
+  },
+  ADD_CONTACT: (state, payload) => {
+    state.contact.unshift(payload);
+  },
+
+  CONTACT: (state, payload) => {
+    state.contact = payload;
   },
 
   actions: {
@@ -36,7 +44,24 @@ export const store = new Vuex.Store({
         })
         .catch(() => {
           this.$swal("Oh oo!", "Could not add the movie!", "error");
-        });
+      })},
+      addContact: (context, payload) => {
+        return axios({
+          method: "post",
+          data: payload,
+          url: "/contact",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => {
+            context.commit("ADD_CONTACT", response.data);
+            this.$swal("Great!", "We received your message!", "success");
+          })
+          .catch(() => {
+            this.$swal("Oh oo!", "We didn't get your message!", "error");
+          });
+      },
     },
 
     fetchMovies: (context, payload) => {
@@ -48,6 +73,14 @@ export const store = new Vuex.Store({
           context.commit("MOVIES", response.data.movies);
         })
         .catch(() => {});
-    }
-  }
-});
+      },
+      fetchContact: (context, payload) => {
+        axios({
+          method: "get",
+          url: "/contact"
+        })
+          .then(response => {
+            context.commit("CONTACT", response.data.contact);
+          })
+          .catch(() => {});
+        }}})
