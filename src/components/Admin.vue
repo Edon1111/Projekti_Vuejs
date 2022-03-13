9c102bc7c4be833224963948355d7a8f29f7c040
Edon1111 <63924185+Edon1111@users.noreply.github.com>
2022-03-13 01:48:10 +0100

    Hide UnHide list of data te Admini

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,92 +1,101 @@
 <template>
   <v-container>
-    <h1>Admin Panel</h1>
-    <h2>List of Movies</h2>
-
-    <v-btn
-      depressed
-      dark
-      color="green"
-      id="add_movie_link"
-      v-bind:to="{ name: 'AddMovie' }"
+    <h1>ADMIN DASHBOARD</h1>
+    <v-btn @click="show = !show">List of Movies</v-btn>
+    <v-btn @click="show_1 = !show_1"
+      >List of Messages delivered from Contact Page</v-btn
     >
-      Add Movie
-    </v-btn>
+    <v-btn @click="show_3 = !show_3">List of Ratings</v-btn>
+    <v-btn @click="show_2 = !show_2">List of Registered Users</v-btn>
 
-    <div class="flex-table">
-      <div>Name</div>
-      <div>Release Year</div>
-      <div>Genre</div>
-      <div>Original Language</div>
-      <div>Run Time</div>
-      <div>Description</div>
-      <div>Actions</div>
-    </div>
-    <div v-for="movie in movies" :key="movie.id" class="flex-table">
-      <div>{{ movie.name }}</div>
-      <div>{{ movie.release_year }}</div>
-      <div>{{ movie.genre }}</div>
-      <div>{{ movie.originalLanguage }}</div>
-      <div>{{ movie.runTime }}</div>
-      <div>{{ movie.moreDescription | abbreviate }}</div>
-      <div class="actions">
-        <v-btn x-small v-bind:to="`/movies/${movie._id}`">Show</v-btn>
-        <v-btn x-small>
-          <router-link :to="{ name: 'EditMovie', params: { id: movie.id } }"
-            >Edit</router-link
-          ></v-btn
-        >
-        <v-btn x-small @click="deleteMovie(movie)">Delete</v-btn>
+    <div v-if="show">
+      <v-btn
+        depressed
+        dark
+        color="green"
+        id="add_movie_link"
+        v-bind:to="{ name: 'AddMovie' }"
+      >
+        Add Movie
+      </v-btn>
+
+      <div class="flex-table">
+        <div>Name</div>
+        <div>Release Year</div>
+        <div>Genre</div>
+        <div>Original Language</div>
+        <div>Run Time</div>
+        <div>Description</div>
+        <div>Actions</div>
+      </div>
+      <div v-for="movie in movies" :key="movie.id" class="flex-table">
+        <div>{{ movie.name }}</div>
+        <div>{{ movie.release_year }}</div>
+        <div>{{ movie.genre }}</div>
+        <div>{{ movie.originalLanguage }}</div>
+        <div>{{ movie.runTime }}</div>
+        <div>{{ movie.moreDescription | abbreviate }}</div>
+        <div class="actions">
+          <v-btn x-small v-bind:to="`/movies/${movie._id}`">Show</v-btn>
+          <v-btn x-small>
+            <router-link :to="{ name: 'EditMovie', params: { id: movie.id } }"
+              >Edit</router-link
+            ></v-btn
+          >
+          <v-btn x-small @click="deleteMovie(movie)">Delete</v-btn>
+        </div>
       </div>
     </div>
 
-    <h1>List of Messages delivered from Contact Page</h1>
     <br />
     <hr />
     <br />
-    <div class="flex-table">
-      <div>Name</div>
-      <div>Surname</div>
-      <div>Message</div>
-      <div>Email</div>
-    </div>
-    <div v-for="contact in contacts" :key="contact.id" class="flex-table">
-      <div>{{ contact.name }}</div>
-      <div>{{ contact.surname }}</div>
-      <div>{{ contact.message }}</div>
-      <div>{{ contact.email }}</div>
-    </div>
 
-    <br /><br />
-    <hr />
-    <br /><br />
-
-    <h1>List of Registered Users</h1>
-
-    <div class="flex-table">
-      <div>Email</div>
-      <div>Password</div>
-    </div>
-    <div v-for="user in users" :key="user.id" class="flex-table">
-      <div>{{ user.email }}</div>
-      <div>{{ user.password | abbreviate }}</div>
+    <div v-if="show_1">
+      <div class="flex-table">
+        <div>Name</div>
+        <div>Surname</div>
+        <div>Message</div>
+        <div>Email</div>
+      </div>
+      <div v-for="contact in contacts" :key="contact.id" class="flex-table">
+        <div>{{ contact.name }}</div>
+        <div>{{ contact.surname }}</div>
+        <div>{{ contact.message }}</div>
+        <div>{{ contact.email }}</div>
+      </div>
     </div>
 
     <br /><br />
     <hr />
     <br /><br />
 
-    <h2>List of Ratings</h2>
-
-    <div class="flex-table">
-      <div>Movie_id</div>
-      <div>User</div>
-      <div>Rate</div>
+    <div v-if="show_2">
+      <div class="flex-table">
+        <div>Email</div>
+        <div>Password</div>
+      </div>
+      <div v-for="user in users" :key="user.id" class="flex-table">
+        <div>{{ user.email }}</div>
+        <div>{{ user.password | abbreviate }}</div>
+      </div>
     </div>
-    <div v-for="rating in ratings" :key="rating.id" class="flex-table">
-      <div>{{ rating.movie_id }}</div>
-      <div>{{ rating.user_id }}</div>
-      <div>{{ rating.rate }}</div>
+
+    <br /><br />
+    <hr />
+    <br /><br />
+
+    <div v-if="show_3">
+      <div class="flex-table">
+        <div>Movie_id</div>
+        <div>User</div>
+        <div>Rate</div>
+      </div>
+      <div v-for="rating in ratings" :key="rating.id" class="flex-table">
+        <div>{{ rating.movie_id }}</div>
+        <div>{{ rating.user_id }}</div>
+        <div>{{ rating.rate }}</div>
+      </div>
     </div>
   </v-container>
 </template>
@@ -94,6 +103,14 @@
 <script>
 import { mapState } from "vuex";
 export default {
+  data() {
+    return {
+      show: false,
+      show_1: false,
+      show_2: false,
+      show_3: false
+    };
+  },
   mounted() {
     this.$store.dispatch("fetchContacts");
     this.$store.dispatch("fetchMovies");
@@ -142,7 +159,7 @@ export default {
 <style scoped>
 .flex-table {
   display: grid;
-  grid-template-columns: repeat(auto-fill, 14%);
+  grid-template-columns: repeat(auto-fill, 12%);
   padding: 10px;
   border-bottom: 1px black solid;
   border-collapse: collapse;
