e8af0cae7468fdeb52320b14b0effedee63613d5
Edon1111 <63924185+Edon1111@users.noreply.github.com>
2022-03-14 21:31:23 +0100

    Revert "home content"
    This reverts commit 657e2dfd0da64f7ccbbe41bf655b112939424e79.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,160 +1,198 @@
 <template>
-   <body>
-  
-     <v-carousel hide-delimiters>
-    <v-carousel-item
-      v-for="(item,i) in photo"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
-          <v-container>
-           <v-layout justify-center > 
-             <v-btn link to="/users/login" v-if="!current_user" depressed color="red darken-4" class="white--text">Read & rate a movie</v-btn>
-              <v-btn link to="/movies" v-else-if="current_user" depressed  color="red darken-4" class="white--text">Read & rate a movie</v-btn>
-           </v-layout>
-          </v-container>
-      <v-divider></v-divider>
-     
-     <v-container>
-       <v-layout row wrap justify-space-around>
-         <v-flex xs6 md4>
-           
-          <v-list-item-title class="headline title mb-1">About Us</v-list-item-title>
-          <strong white--text>-</strong>
-          <div>This is an online database of information related to films,television series, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews</div>
-         </v-flex>
+  <v-app>
+    <v-container>
+      <section>
+        <v-carousel hide-delimiters>
+          <v-carousel-item
+            v-for="(item, i) in photo"
+            :key="i"
+            :src="item.src"
+          ></v-carousel-item>
+        </v-carousel>
+        <section id="movies">
+          <div class="coach">
+            <v-btn class="butoniRead" link to="/movies" color="red darken-4"
+              >Read & rate a movie</v-btn
+            >
+          </div>
+        </section>
+      </section>
 
-        <v-flex xs4 md2>
-           <v-img max-height="209" max-width="262"
-            src="https://powping.com/file/9c3a26bb37764d887fcef6646e4f237aa7995a2a21abdfb6e0591d87d063bc65"></v-img>
-        </v-flex>
-       </v-layout>
-     </v-container>
+      <section id="about">
+        <div class="row">
+          <div class="column">
+            <p>Enjoy on your TV.</p>
+            <div class="p">
+              <h4 style="padding-left:15px; color: #787878;">
+                Watch on Smart TVs, Playstation, Xbox, Chromecast,<br />
+                Apple TV, Blu-ray players, and more.
+              </h4>
+            </div>
+          </div>
+          <div class="column">
+            <img
+              src="https://powping.com/file/9c3a26bb37764d887fcef6646e4f237aa7995a2a21abdfb6e0591d87d063bc65"
+              style="padding-left:60px; padding-top:50px; max-width: 90%;
+     max-height: 90%;"
+            />
+          </div>
+        </div>
+        <br /><br />
+      </section>
 
-      
-
-      <v-layout justify-center>
-          <v-timeline
-          align-top
-          dense
-        >
-  
-          <v-timeline-item
-            color="black"
-            small
-          >
-            <v-layout pt-3>
-              <v-flex xs3>
-                <strong>Profiles for kids.</strong>
-              </v-flex>
-              <v-flex>
-                <div class="subheading">Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</div>
-                <v-container>
-                  <v-avatar size="110" tile>
-                  <v-img
-                    src="https://t3.ftcdn.net/jpg/02/94/94/78/360_F_294947844_akUXUJ4eiylpWhjJ1cP63Gq0iMrEOHno.jpg"
-                  ></v-img>
-                </v-avatar>
-                </v-container>
-                
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-  
-          <v-timeline-item
-            color="red darken-4"
-            small
-          >
-            <v-layout pt-3>
-              <v-flex xs3>
-                <strong>Watch everywhere.</strong>
-              </v-flex>
-              <v-flex>
-                <div class="subheading">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</div>
-                <v-container>
-                  <v-avatar size="110" tile>
-                  <v-img
-                    src="https://lycom.co/wp-content/uploads/2018/04/watch-tv-everywhere.jpg"
-                  ></v-img>
-                </v-avatar>
-                </v-container>
-                
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-
-          <v-timeline-item
-            color="black"
-            small
-          >
-            <v-layout pt-3>
-              <v-flex xs3>
-                <strong>Enjoy on your TV.</strong>
-              </v-flex>
-              <v-flex>
-                <div class="subheading">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</div>
-                <v-container>
-                  <v-avatar size="110" tile>
-                  <v-img
-                    src="https://www.usnews.com/dims4/USNEWS/fe2964d/2147483647/crop/1996x1310%2B3%2B0/resize/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F9b%2Fd6%2F7498ee6440d1b6d5b2626c11ec46%2F200723-stock.jpg"
-                  ></v-img>
-                </v-avatar>
-                </v-container>
-                
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-
-        </v-timeline>
-      </v-layout>
-          
-   </body>
-
+      <section>
+        <v-layout column wrap class="my-5" align-center>
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">
+                The best place to enjoy the most amazing movies
+              </h2>
+              <span class="subheading">
+                No more restrictions, no more limits
+              </span>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-4"
+                        >mdi-movie-play</v-icon
+                      >
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">
+                        Download your best movies and watch it everywhere.
+                      </div>
+                    </v-card-title>
+                    <v-card-text>
+                      Watching a movie wasnt never so easily.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-4"
+                        >mdi-video-vintage</v-icon
+                      >
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Create profiles for kids.</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Send kids on adventures with their favorite characters in
+                      a space made just for them—free with your membership.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="red--text text--darken-4"
+                        >mdi-movie-roll</v-icon
+                      >
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">
+                        Download your shows to watch offline.
+                      </div>
+                    </v-card-title>
+                    <v-card-text>
+                      Save your favorites easily and always have something to
+                      watch.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import axios from "axios";
-  export default {
-    data() {
+export default {
+  data() {
     return {
-      current_user: null,
       photo: [
-          {
-            src: 'https://cdn.thetealmango.com/wp-content/uploads/2021/09/expensive-films.jpg',
-          },
-          {
-            src: 'https://images.summitmedia-digital.com/esquiremagph/images/2020/04/07/Top-Rated-Trinity-01.jpg',
-          },
-          {
-            src: 'https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1579830754870-9IAYNSOR0S7TP5SKI9VR/image-asset.jpeg',
-          },
-          {
-            src: 'https://unrealitymag.com/wp-content/uploads/2013/01/et3.jpg',
-          },
-        ],
+        {
+          src:
+            "https://cdn.thetealmango.com/wp-content/uploads/2021/09/expensive-films.jpg"
+        },
+        {
+          src:
+            "https://images.summitmedia-digital.com/esquiremagph/images/2020/04/07/Top-Rated-Trinity-01.jpg"
+        },
+        {
+          src:
+            "https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1579830754870-9IAYNSOR0S7TP5SKI9VR/image-asset.jpeg"
+        },
+        {
+          src: "https://unrealitymag.com/wp-content/uploads/2013/01/et3.jpg"
+        }
+      ]
     };
-  },
-    mounted() {
-    this.fetchUser();
-  },
-  methods:{
-     async fetchUser() {
-      return axios({
-        method: "get",
-        url: "/api/current_user"
-      })
-        .then(response => {
-          this.current_user = response.data.current_user;
-        })
-        .catch(() => {
-          this.$router.push({ name: "Movies" });
-        });
-    },
-
   }
-
-  }
+};
 </script>
 
+<style scoped>
+section {
+  width: 100%;
+  float: left;
 
+  position: relative;
+}
+#movies {
+  background-color: rgb(255, 255, 255);
+}
+
+#about {
+  background-color: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: baseline;
+}
+
+.row {
+  display: flex;
+}
+.column {
+  flex: 50%;
+}
+p {
+  position: absolute;
+  color: #484848;
+  left: 110px;
+  top: 170px;
+  font-size: 220%;
+}
+.p {
+  position: absolute;
+  color: #484848;
+  left: 100px;
+  top: 220px;
+}
+.p1 {
+  position: absolute;
+  color: #484848;
+  left: 630px;
+  top: 650px;
+}
+.social-icon {
+  font-size: 21px;
+  color: white;
+}
+.butoniRead {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  margin-top: 20px;
+}
+</style>
