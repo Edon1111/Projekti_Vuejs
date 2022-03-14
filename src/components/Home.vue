ec8439f3c24fa278d657123a06d5f4e76c90214f
anilaavdylii <aa48857@ubt-uni.net>
2022-03-14 20:23:43 +0100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,6 +21,7 @@
          <v-flex xs6 md4>
            
           <v-list-item-title class="headline title mb-1">About Us</v-list-item-title>
+          <strong white--text>-</strong>
           <div>This is an online database of information related to films,television series, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews</div>
          </v-flex>
 
@@ -31,9 +32,82 @@
        </v-layout>
      </v-container>
 
-      <v-divider></v-divider>
+      
 
-     
+      <v-layout justify-center>
+          <v-timeline
+          align-top
+          dense
+        >
+  
+          <v-timeline-item
+            color="black"
+            small
+          >
+            <v-layout pt-3>
+              <v-flex xs3>
+                <strong>Profiles for kids.</strong>
+              </v-flex>
+              <v-flex>
+                <div class="subheading">Send kids on adventures with their favorite characters in a space made just for them—free with your membership.</div>
+                <v-container>
+                  <v-avatar size="110" tile>
+                  <v-img
+                    src="https://t3.ftcdn.net/jpg/02/94/94/78/360_F_294947844_akUXUJ4eiylpWhjJ1cP63Gq0iMrEOHno.jpg"
+                  ></v-img>
+                </v-avatar>
+                </v-container>
+                
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+  
+          <v-timeline-item
+            color="red darken-4"
+            small
+          >
+            <v-layout pt-3>
+              <v-flex xs3>
+                <strong>Watch everywhere.</strong>
+              </v-flex>
+              <v-flex>
+                <div class="subheading">Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</div>
+                <v-container>
+                  <v-avatar size="110" tile>
+                  <v-img
+                    src="https://lycom.co/wp-content/uploads/2018/04/watch-tv-everywhere.jpg"
+                  ></v-img>
+                </v-avatar>
+                </v-container>
+                
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item
+            color="black"
+            small
+          >
+            <v-layout pt-3>
+              <v-flex xs3>
+                <strong>Enjoy on your TV.</strong>
+              </v-flex>
+              <v-flex>
+                <div class="subheading">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</div>
+                <v-container>
+                  <v-avatar size="110" tile>
+                  <v-img
+                    src="https://www.usnews.com/dims4/USNEWS/fe2964d/2147483647/crop/1996x1310%2B3%2B0/resize/640x420/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F9b%2Fd6%2F7498ee6440d1b6d5b2626c11ec46%2F200723-stock.jpg"
+                  ></v-img>
+                </v-avatar>
+                </v-container>
+                
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+        </v-timeline>
+      </v-layout>
           
    </body>
 
